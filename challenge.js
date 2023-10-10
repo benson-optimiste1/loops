51b431dfe8af3d95ed5657c5167e001686fab89f
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-10-10 10:53:24 -0400

    loops trough array

---
+++ challenge.js
@@ -0,0 +1,6 @@
+const videos = ['cats', 'news', 'games', 'sports']
+// console log each of these videos to the screen
+// Make sure it works for any lenght of array
+for(let i = 0; i < videos.length; i++){
+    console.log(videos[i])
+}
---
+++ loops.js
@@ -0,0 +1,26 @@
+// Say hello 3 times
+for (let i = 0; i < 3 ; i++){
+    console.log('hello')
+}
+       
+/*
+0
+1
+2
+3
+*/
+
+for(let i = 0; i < 4; i++){
+    console.log(i)
+}
+
+const people = ['Evelyne', 'Glissie', 'Benson', 'Derek', 'Todd',]
+// make a loop that says hello to each person in the array
+for(let i = 0; i < people.length; i++){
+    console.log(' hello ' + people[i])
+}
+
+const fruits = ['apple', 'banana', 'grape']
+for(let i = array.length-1; i >= 0 ; i--){
+    console.log(fruits[i])
+}
